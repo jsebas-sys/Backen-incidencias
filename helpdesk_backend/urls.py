ad07f17ad175630d8c6c57a206ad00f708726ac7
Sebastián <sebastian@example.com>
2025-05-05 19:35:10 -0500

    gg

--- helpdesk_backend/urls.py
+++ helpdesk_backend/urls.py
@@ -1,7 +1,8 @@
+# helpdesk_backend/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  
-    path('incidencias/', include('incidencias.urls')),  
+    path('admin/', admin.site.urls),  # Ruta del panel de administración
+    path('incidencias/', include('incidencias.urls')),  # Incluye las rutas de la app incidencias
 ]
