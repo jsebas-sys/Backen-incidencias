d0e2a305d872498cdc675b60f6f9356f54f22479
Sebastián <sebastian@example.com>
2025-05-26 17:53:23 -0500

    bgbg

--- incidencias/serializers.py
+++ incidencias/serializers.py
@@ -4,5 +4,5 @@ from .models import Incidencia
 class IncidenciaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Incidencia
-        fields = ['id', 'codigo', 'equipo', 'fecha', 'estado', 'prioridad', 'usuario', 'detalle']
+        fields = ['id', 'codigo', 'equipo', 'fecha', 'updated_at', 'estado', 'prioridad', 'usuario', 'detalle']
         read_only_fields = ['usuario']
